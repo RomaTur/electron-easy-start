84698e9f6630c6e4e464d0de12f0d1e0e63eefbb
Roman Turusov <borka.grr@gmail.com>
2018-04-02 13:23:47 +0300

    particlesBg added

---
+++ src/Components/App/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import App from './App';
+
+it('renders without crashing', () => {
+  Enzyme.shallow(<App />);
+});
